ca6ace3229f37b353e2965c7f74a06c5ac17bb45
DomMarcone <dammarcone@yahoo.com>
2019-08-25 17:23:39 -0400

    Removed precision_t.h from .gitignore. It was originally going to be pragmatically generated with a precision_t.h.in

---
+++ inc/precision_t.h
@@ -0,0 +1,19 @@
+//precision_t.h.in
+#pragma once
+#ifndef _PRECISION_T_H
+#define _PRECISION_T_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif //__cplusplus
+
+
+//set precision below
+typedef double precision_t;
+
+
+#ifdef __cplusplus
+}
+#endif //__cplusplus
+
+#endif //_PRECISION_T_H
